a8f213087819dc3c15b5f5ee8094019bb1f3704c
ayo986 <140111670+ayo986@users.noreply.github.com>
2024-12-07 12:58:35 +0100

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,290 +1,259 @@
 class itemText extends moveClip{
-    origin = CENTER_CENTER
-    text = ""
-    outLine = false
-    toggel = {
-        defualt: [200, 100, 50],
-        other: [50, 100, 200],
+  origin = CENTER_CENTER
+  text = ""
+  outLine = false
+  hasBg = false
+  alignText = 'center'
+  iconSize = 0
+  fontSize = 18
+  corner = [20]
+  opacity = 20
+  bg = color(200)
+  fg = color(0)
+  toggel = {
+      defualt: [200, 100, 50],
+      other: [50, 100, 200],
+  }
+  textColor = [200]
+  ncount = 0
+
+  constructor(text, x, y, w, h){
+    super(x, y, w, h)
+    this.text = text
+
+    let timer = new Text()
+    timer.set({
+      shape: 2,
+      origin: CENTER_CENTER,
+      shadowBlur: 10,
+      shadowColor: 'hsl(100, 50%, 70%)',
+      mousepressed:function(x, y, b){
+          let p = this.Parent()
+          if (b == 1) {
+              this.opacity = 50
+              if (p.ncount > 0) {
+                  p.ncount --
+              }
+          }
+      },
+
+      mousereleased:function(x, y, b){
+          this.opacity = 100
+      }
+    })
+    this.timer = timer
+    this.pack(timer)
+
+    ///////////////////////////////
+    let one = new Text('+', 0,  0, 30, 30)
+    one.set({
+      shape: 2,
+        bg: 'hsl(180, 30%, 40%)',
+        fg: color(200),
+        fontSize: 16,
+    })
+    one.mousepressed = function(x, y, b){
+      let p = this.Parent().Parent()
+      if (b == 1) {
+          this.opacity = 50
+          p.fontSize = p.fontSize + 1
+      }
     }
-    textColor = [200]
-    ncount = 40
-    // timeontap = 60
-    constructor(text, x, y, w, h){
-      super(x, y, w, h)
-      this.text = text
-      let [ox, oy] = this.Origin()
-      let [ow, oh] = this.Size()
-      let b = new Text()
-      b.set({
-        shape: 2,
-        origin: CENTER_CENTER,
-        shadowBlur: 10,
-        shadowColor: 'hsl(100, 50%, 70%)',
-        mousepressed:function(x, y, b){
-            let p = this.Parent()
-            if (b == 1) {
-                this.opacity = 50
-                if (p.ncount > 0) {
-                    p.ncount --
-                }
-            }
-        },
-
-        mousereleased:function(x, y, b){
-            this.opacity = 100
-        }
-      })
-      this.pack(b)
-
-      let one = new Text('+', 0,  0, 30, 30)
-      one.set({
-        shape: 2,
-          bg: 'hsl(180, 30%, 40%)',
-          fg: color(200),
-          fontSize: 16,
-      })
-      one.mousepressed = function(x, y, b){
-        let p = this.Parent().Parent()
-        if (b == 1) {
-            this.opacity = 50
-            p.fontSize = p.fontSize + 1
-        }
-      }
-
-      one.mousereleased = function(x, y, b){
-        this.opacity = 100
-      }
-
-      let two = new Text('-', 0,  0, 30, 30)
-      two.set({
-          shape: 2,
-          bg: 'hsl(180, 30%, 40%)',
-          fg: color(200),
-          fontSize: 16,
-      })
-      two.mousepressed = function(x, y, b){
-        let p = this.Parent().Parent()
-        if (b == 1) {
-            this.opacity = 50
-            p.fontSize = p.fontSize - 1
-        }
-      }
-
-      two.mousereleased = function(x, y, b){
-        this.opacity = 100
-      }
-      this.timer = b
-
-      let group = new listMenuView(0, 0, 80, 40)
-      group.direction = 'h'
-      group.hasBg = false
-      group.displayItems = 2
-      this.group = group
-      
-      this.group.addItem(two)
-      this.group.addItem(one)
-      this.pack(group)
+    one.mousereleased = function(x, y, b){
+      this.opacity = 100
     }
+    let two = new Text('-', 0,  0, 30, 30)
+    two.set({
+        shape: 2,
+        bg: 'hsl(180, 30%, 40%)',
+        fg: color(200),
+        fontSize: 16,
+    })
+    two.mousepressed = function(x, y, b){
+      let p = this.Parent().Parent()
+      if (b == 1) {
+          this.opacity = 50
+          p.fontSize = p.fontSize - 1
+      }
+    }
+    two.mousereleased = function(x, y, b){
+      this.opacity = 100
+    }
+    
+    let group = new listMenuView(0, 0, 80, 40)
+    group.direction = 'h'
+    group.hasBg = false
+    group.displayItems = 2
+    this.group = group
+    this.group.addItem(two)
+    this.group.addItem(one)
+    this.pack(group)
+  }
+  mupdate(){
+    let [ox, oy] = this.Origin()
+    let [w, h] = this.Size()
+    let r = 20
+    let xx = (-ox + w ) * 0.95- r - 10
+    let yy = -oy  + r *2 + 10
+    let rgb = this.ncount > 0 ? this.toggel.defualt : this.toggel.other
+    this.timer.set({
+      x: xx,
+      y: yy,
+      width: r*2,
+      height:r*2,
+      bg: rgb,
+      fg: this.textColor,
+      text: this.ncount,
+      fontSize: 14,
+      toggel: {
+          defualt: [200, 100, 50],
+          other: [50, 100, 200]
+      }
+    })
 
-    draw(){
-      let alpha = (this.opacity/100) * 255
-      textAlign(CENTER, TOP)
-      textFont(this.font)
-      textSize(this.fontSize)
-      rectMode(CENTER)
-      imageMode(CENTER)
-      let [ox, oy] = this.Origin()
-      let [w, h] = this.Size()
-      let c = color(this.fg)
-      c.setAlpha(alpha)
+    this.group.set({
+      x: (-ox + w/2) - w*0.95/2 + this.group.width/2 + 10,
+      y: this.timer.y
 
-      noStroke()
-      push()
-      drawingContext.save()
-      fill(255, 50)
-      rect(-ox+w/2 , -oy+h/2 , this.width * 0.95, this.height * 0.9)
-      tint(this.imageColor)
+    })
+  }
+  draw(){
+    this.mupdate()
+    textAlign(CENTER, TOP)
+    textFont(this.font)
+    textSize(this.fontSize)
+    rectMode(CENTER)
+    imageMode(CENTER)
+    let [ox, oy] = this.Origin()
+    let [w, h] = this.Size()
+    noStroke()
+    drawingContext.save()
+    let a = this.corner[0] | 0
+    let b = this.corner[1] | a 
+    let c = this.corner[2] | a 
+    let d = this.corner[3] | a
+    fill(160, 80)
+    rect(-ox+w/2 , -oy+h/2 , this.width * 0.95, this.height * 0.9, a, b, c, d)
+    drawingContext.clip()
+    if (this.image) {
+      tint(this.imageColor || 255)
       image(this.image, -ox + w/2, -oy + h/2, this.width * 0.95, this.height * 0.9)
-      drawingContext.clip()
-
-      let rgb = this.ncount > 0 ? this.toggel.defualt : this.toggel.other
-      // fill(rgb)
-      let r = 20
-      let xx = (-ox + w ) * 0.95- r - 10
-      let yy = -oy  + r *2 + 10
-      this.timer.set({
-        x: xx,
-        y: yy,
-        width: r*2,
-        height:r*2,
-        bg: rgb,
-        fg: this.textColor,
-        text: this.ncount,
-        fontSize: 14,
-        toggel: {
-            defualt: [200, 100, 50],
-            other: [50, 100, 200]
-        }
-      })
-
-      this.group.set({
-        x: (-ox + w/2) - w*0.95/2 + this.group.width/2 + 10,
-        y: this.timer.y
-
-      })
-      noStroke()
-      noFill()
-      if (this.outLine) {
-        stroke(c)
-      }else{
-        fill(c)
-      }
-      // let hh = textHeight(this.text)
-      textSize(this.fontSize)
-      text(
-        this.text, -ox+w/2 , -oy+h/2 + this.height*0.1 , this.width * 0.8, this.height * 0.9)
-
-        drawingContext.restore()
-        pop()
     }
+    fill(this.fg)
+    textSize(this.fontSize)
+    text(this.text, -ox+w/2 , -oy+h/2 + this.height*0.1 , this.width * 0.8, this.height * 0.9)
+    drawingContext.restore()
+  }
 }
 
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+let bg, lv, tit, menu, adhkarbg
 
-let bg 
+
+function loadItem(item) {
+  lv.items = []
+  lv.childs = []
+  lv.acc = 0
+  tit.text =  '/' + item.array.length + '/ ' + item.text +  String.fromCharCode(9664)
+  for (const k in item.array) {
+      let v = item.array[k]
+      let b = new itemText()
+      b.text = '/' + (parseInt(k)+1) + '/\r\n' + v.text 
+      b.ncount = v.count
+      b.image = bg
+      // b.imageColor = color(0, 30)
+      lv.addItem(b)
+  }
+}
+
 let scene = new Scene()
 scene.setup = function(){
-    this.childs = []
-    bg = newImage('res/ibg5.jpg')
-    this.bg = 'hsl(180, 20%, 40%)'
-
-    let tit = new Button({
-        text: '',
-        width: width,
-        height: 60,
-        x: cx,
-        y: 30,
-        alignText: 'center',
-        fontSize: 18,
-        corner:[],
-        // hasBg: false,
-        bg: 'hsl(180, 60%, 20%)'
-    })
-    this.pack(tit)
+  this.childs = []
+  bg = newImage('res/ibg9.jpg')
+  adhkarbg = newImage('res/adhkarbg.jpg')
+  this.bg = 'hsl(180, 40%, 40%)'
+  this.bgImage = adhkarbg
+  
 
 
+  tit = new Button({
+      text: '',
+      width: width,
+      height: 60,
+      x: cx,
+      y: 30,
+      alignText: 'center',
+      fontSize: 18,
+      corner:[],
+      // hasBg: false,
+      bg: 'hsl(180, 60%, 40%)',
+      fg: [0]
+  })
+  this.pack(tit)
+  let hh = height - 120
+  lv = new listMenuView(cx, hh/2 + 120, width, hh)
+  lv.direction = 'v'
+  lv.hasBg = false
+  lv.displayItems = 1
+  lv.distanceofmove = 0
+  lv.friction = 1
+  lv.lineColor = 'hsl(30, 50%, 50%)'
+  this.pack(lv)
+  menu = new menuView(cx, 90, width, 60)
+  menu.displayItems = 7
+  menu.lastIndex = -1
+  menu.bg = 'hsl(180, 40%, 50%)'
+  // menu.hasBg = false
+  menu.distanceofmove = 0
+  menu.friction = 0.2
+  menu.lineColor = 'hsl(30, 50%, 50%)'
+  menu.space = 16
+  menu.showLine = false
+  menu.indexValue = 1
+  menu.onChange = function() {
 
-    let hh = height - 120
-    let lv = new listMenuView(cx, hh/2 + 120, width, hh)
-    lv.direction = 'v'
-    lv.hasBg = false
-    lv.displayItems = 1
-    lv.distanceofmove = 0
-    lv.friction = 1
-    lv.lineColor = 'hsl(30, 50%, 50%)'
-    this.pack(lv)
-    let menu = new menuView(cx, 90, width, 60)
-    menu.displayItems = 7
-    menu.lastIndex = -1
-    menu.bg = 'hsl(180, 50%, 30%)'
-    // menu.hasBg = false
-    menu.distanceofmove = 0
-    menu.friction = 0.2
-    menu.lineColor = 'hsl(30, 50%, 50%)'
-    menu.space = 16
-    menu.showLine = false
-    menu.indexValue = 1
-    menu.onChange = function() {
-
-    }
-    this.pack(menu)
-
-    function loadItem(item) {
-        lv.items = []
-        lv.childs = []
-
-        let [w, h] = item.Size()
-        lv.acc = 0
-        tit.text =  '/' + item.array.length + '/ ' + item.text +  String.fromCharCode(9664)
-        for (const k in item.array) {
-            // print(k, item.array[k])
-
-            let v = item.array[k]
-            let b = new itemText()
-            b.alignText = 'center'
-            b.iconSize = 0
-            b.text = '/' + (parseInt(k)+1) + '/\r\n' + v.text 
-            b.ncount = v.count
-            b.image = bg
-            // b.bgColorImage = color(200)
-            // b.clip = false
-            // b.shape = 2
-            b.bg = 'hsl(180, 20%, 45%)'
-            b.fg = [0]
-            // b.hasBg = false
-            b.imageColor = color(255, 150)
-            b.fontSize = 21
-            // b.corner = [20]
-            lv.addItem(b)
-
-            
-        }
-    }
-    loadJSON('adhkar/adhkar.json',(e)=>{
-        for (let i = 0; i < e.length; i++) {
-            const v = e[i];
-            let b = new Text()
-            b.indexItem = i + 1
-            b.alignText = 'center'
-            b.iconSize = 0
-            b.text = v.category 
-            // b.clip = false
-            // b.shape = 2
-            b.bg = 'hsl(180, 60%, 20%)'
-            b.fg = [200]
-            b.corner = [20]
-            b.fontSize = 12
-            b.array = v.array
-            b.update = function(){
+  }
+  this.pack(menu)
+  loadJSON('adhkar/adhkar.json',(e)=>{
+      for (let i = 0; i < e.length; i++) {
+          const v = e[i];
+          let b = new Text()
+          b.indexItem = i + 1
+          b.alignText = 'center'
+          b.iconSize = 0
+          b.text = v.category 
+          b.bg = 'hsl(180, 50%, 60%)'
+          b.fg = [0]
+          b.corner = [20]
+          b.fontSize = 12
+          b.array = v.array
+          // b.hasBg = false
+          b.update = function(){
+            let p = this.Parent()
+            if (p && p.indexValue == this.indexItem) {
+              this.stroke = 'hsl(30, 60%, 60%)'
+              this.strokeWidth = 2
+              this.hasBg = true
+            }else{
+              this.stroke = null
+              this.hasBg = false
+            }
+          }
+          b.ontap = function(){
               let p = this.Parent()
-              if (p && p.indexValue == this.indexItem) {
-                this.stroke = 'hsl(30, 60%, 60%)'
-                this.strokeWidth = 2
-              }else{
-                this.stroke = null
+              let item = this
+              if (!p.moving) {
+                p.indexValue = this.indexItem
+                loadItem(item)
               }
-            }
-            b.ontap = function(){
-                let p = this.Parent()
-                let item = this
-                if (!p.moving) {
-                  p.indexValue = this.indexItem
-                  loadItem(item)
-                }
-            }
-            b.mousepressed = function(x, y, b){
-                
-                if (b == 1) {
-                    // this.opacity = 50
-                }
-            }
-
-            b.mousereleased = function() {
-                this.opacity = 100
-            }
-            b.onEnter = function(){
-    
-            }
-            b.onLeave = function(){
-              this.opacity = 100
-
-            }
-            menu.addItem(b)
-        }
-        loadItem(menu.items[0])
-    })    
-    menu.setIndex(1)
-
-    
+          }
+          menu.addItem(b)
+      }
+      loadItem(menu.items[0])
+  })    
+  menu.setIndex(1)
 }
 
 return scene
